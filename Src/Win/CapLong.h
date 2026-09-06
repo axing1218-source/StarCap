5e6bad9f5f6dff719571633502344df11f718163
axing1218-source <axing1218@gmail.com>
2026-09-07 03:43:09 +0800

    Add adaptive long-capture auto-scroll state

--- Src/Win/CapLong.h
+++ Src/Win/CapLong.h
@@ -41,10 +41,26 @@ public:
 		return true;
 	}
 private:
+	enum class AutoScrollStrategy
+	{
+		SendInput = 0,
+		ChildWheelMessage = 1,
+		RootWheelMessage = 2,
+	};
+
 	void firstStep();
 	void makeImgPreview();
 	void capStep();
+	void processFrame(std::vector<BYTE> data);
 	void scheduleNextCapture(int delayMs = 120);
+	void scheduleAutoScroll(int delayMs = 80);
+	void dispatchAutoScroll();
+	void sampleAutoSettle();
+	void handleAutoNoProgress();
+	bool advanceAutoScrollStrategy();
+	void fallbackToManual();
+	void resolveAutoTargets();
+	const wchar_t* autoStrategyName() const;
 	void makeTool();
 	void paintImgPreview(ID2D1DeviceContext* ctx);
 	void stopCap();
@@ -53,23 +69,28 @@ private:
 	WinCap* win;
 	bool isCapturing{ false }, isFinish{ false }, autoScroll{ false };
 	bool firstCheck{ true };
+	bool autoStrategyConfirmed{ false };
 	int dismissTime{ 0 };
 	int settleRecheckCount{ 0 };
+	int autoSettleChecks{ 0 };
 	int changeStartY{ -1 };
+	AutoScrollStrategy autoStrategy{ AutoScrollStrategy::SendInput };
 	D2D1_RECT_F stopTextRect{};
 	D2D1_POINT_2F stopTextPos{};
 	Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> textBrush;
 	Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> bgBrush;
 	Microsoft::WRL::ComPtr<IDWriteTextLayout> layoutTextEnd;
+	POINT autoTargetPoint{};
 	HWND targetHwnd{ nullptr };
+	HWND targetRootHwnd{ nullptr };
 	std::unique_ptr<ToolLong> tool;
 	Microsoft::WRL::ComPtr<ID2D1Bitmap1> imgPreview;
 	std::vector<BYTE> imgData;
 	std::vector<BYTE> img1;
+	std::vector<BYTE> autoSettleFrame;
 	int imgW{ 0 }, imgH{ 0 };
 	int resultH{ 0 };
 	POINT capStartPos{};
 };
-
 
 
