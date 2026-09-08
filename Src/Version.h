b3f8fcde4b7bc47be41916088b8c727030d71817
axing1218-source <axing1218@gmail.com>
2026-09-08 13:26:20 +0800

    Prepare StarCap v0.10.5 version

--- Src/Version.h
+++ Src/Version.h
@@ -1,11 +1,11 @@
 #pragma once
 
 // StarCap product version.
-// Stable release: v0.10.4.
-#define STARCAP_FILE_VERSION 0,10,4,0
-#define STARCAP_FILE_VERSION_STR "0.10.4.0"
-#define STARCAP_DISPLAY_VERSION L"v0.10.4"
-#define STARCAP_PUBLIC_VERSION L"v0.10.4"
-#define STARCAP_RELEASE_TAG "v0.10.4"
-#define STARCAP_RUNTIME_VERSION_STR "StarCap/0.10.4"
+// Stable release: v0.10.5.
+#define STARCAP_FILE_VERSION 0,10,5,0
+#define STARCAP_FILE_VERSION_STR "0.10.5.0"
+#define STARCAP_DISPLAY_VERSION L"v0.10.5"
+#define STARCAP_PUBLIC_VERSION L"v0.10.5"
+#define STARCAP_RELEASE_TAG "v0.10.5"
+#define STARCAP_RUNTIME_VERSION_STR "StarCap/0.10.5"
 #define STARCAP_PRERELEASE 0
